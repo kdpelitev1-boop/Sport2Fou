d00c81bf60cc3d1b55205fccd2636632eae8b353
kdpelitev1-boop <kdp.elite.v1@gmail.com>
2025-11-04 17:58:23 +0100

    Refactor server.js to use package.json format

--- server.js
+++ server.js
@@ -1,30 +1,11 @@
-const NodeMediaServer = require('node-media-server');
-
-const config = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 30,
-    ping_timeout: 60
+{
+  "name": "sport2fou",
+  "version": "1.0.0",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
   },
-  http: {
-    port: process.env.PORT || 8000,
-    allow_origin: '*'
-  },
-  trans: {
-    ffmpeg: '/usr/bin/ffmpeg',
-    tasks: [
-      {
-        app: 'live',
-        hls: true,
-        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-        dash: true,
-        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-      }
-    ]
+  "dependencies": {
+    "node-media-server": "^2.6.0"
   }
-};
-
-const nms = new NodeMediaServer(config);
-nms.run();
+}
